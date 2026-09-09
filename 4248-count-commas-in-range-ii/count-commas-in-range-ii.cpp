0863e35b035dedad6e8f2d43406c0f760feaca90
Sameer Kumar <sameer.kumar2024@vitstudent.ac.in>
2026-09-10 00:06:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (99.20%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     long long countCommas(long long n) {
         long long ans=0;
-        int len=log10(n)+1;
         for(long long i=4,j=999;j<n;i+=3,j=j*1000+999){
             ans+=n-j;
         }
